16a87a4fce1a6563b8d7fc76af56dd1956bbb785
nabe1208 <air.love60@gmail.com>
2022-05-12 13:44:05 +0000

    admin_customer_start

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
     @items = Item.all
   end
@@ -7,24 +7,37 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     @item.save
-    @items = Item.find(params[:id])
-    redirect_to item_path(@items.id)
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
     @item = Item.find(params[:id])
+    @genre = @item.genre
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genre = @item.genre
   end
-  
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_items_path
+  end
+
   private
   #編集中
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :image)
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :image)
   end
 end
+
+# admin_items_path -index,create
+# new_admin_item_path -new
+# edit_admin_item_path -edit
+# admin_item_path -show,update(patch,put)
