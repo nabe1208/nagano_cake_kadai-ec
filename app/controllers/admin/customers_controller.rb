c41b2d9d94651f0bdc6c42d98defda7efb326b32
nabe1208 <air.love60@gmail.com>
2022-05-30 03:49:45 +0000

    orderdetail-admin-clear2

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -23,6 +23,6 @@ class Admin::CustomersController < ApplicationController
 
   private
   def customer_params
-    params.require(:customer).permit(:last_name, :last_name_kana, first_name, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
+    params.require(:customer).permit(:last_name, :last_name_kana, :first_name, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
   end
 end
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,5 +2,7 @@ class Admin::HomesController < ApplicationController
 
   def top
     @orders = Order.all
+    # 指定モデル商品作成日から最新の4件
+    @items = Item.order(created_at: :desc).limit(4)
   end
 end
